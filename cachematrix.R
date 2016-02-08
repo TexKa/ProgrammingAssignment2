9af4ce1a3411c6d90bcc5824d11659e6eac6838f
TexKa <kadiralmus@gmail.com>
2016-02-07 22:32:07 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This second programming assignment requires us to write an R function that 
+## is able to cache potentially time-consuming computations.
+## Following functions are used to create a special "vector" and 
+## cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## The first function(makeCacheMatrix) creates a special "vector", 
+## which is really a list containing a function to
+## 1.set the value of the vector
+## 2.get the value of the vector
+## 3.set the value of the mean
+## 4.get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mtrxinv <- NULL
+        set <- function(mtrxinp) {
+                x <<- mtrxinp
+                mtrxinv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) mtrxinv <<- inverse
+    getinverse <- function() mtrxinv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## The following function calculates the mean of the special "vector" 
+## created with the above function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    mtrxinv <- x$getInverse()
+    if (!is.null(mtrxinv)) {
+        message("getting cached data")
+        return(mtrxinv)
+    }
+    mat <- x$get()
+    mtrxinv <- solve(mat, ...)
+    x$setInverse(mtrxinv)
+    mtrxinv
 }
